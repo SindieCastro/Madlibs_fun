3cbe9bec772b69d320a146c23c333a7b4abc399f
Sindie Castro <Sindie Castro>
2021-08-28 14:36:17 -0500

    adding code

---
+++ short_story_madlib.py
@@ -0,0 +1,17 @@
+#Short Story: The Special Computer
+
+#define variables
+pet_name = input("Pet name: ")
+noun1 = input("Noun: ")
+food = input("Food: ")
+emotion = input("Emotion: ")
+
+#add madlib
+madlib = f"Ryan is so excited to begin exploring Python. He bought a new computer and named it {pet_name}. \
+{pet_name} was no ordinary computer. {pet_name} was special in the sense that the the computer was a big \
+{noun1}. One day, Ryan forgot to turn off {pet_name} and the big {noun1} was following Ryan's commands. Ryan \
+had not noticed that {pet_name} was listening to him. {pet_name} printed a bowl of {food}. Ryan was surprised \
+and {emotion}."
+
+#print madlib
+print(madlib)
